50f33edacf1e19f555d193761ce08566d9a0ed54
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-09-09 19:16:43 -0400

    create pages folder that will hold the different pages. Create Homepage.js file that returns the homepage component

---
+++ client/src/pages/homepage.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import Header from "../components/header";
+
+const homepage = () => {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+};
+
+export default homepage;
