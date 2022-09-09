1cd11ae3e7e227fadd3c467d71918aa5b42859c7
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-09-09 19:21:19 -0400

    import BrowserRouter, Routes, and Route components from react-router-dom and render the Homepage component when the path is '/'

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/homepage";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>WORKS!</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Homepage />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
