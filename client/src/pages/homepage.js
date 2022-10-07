343841ee28abbe91d0ef99acc17a787d3a4963f3
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-10-06 23:36:56 -0400

    add offers component to homepage

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import Header from "../components/header";
+import Offers from "../components/offers";
 
 const homepage = () => {
   return (
     <Fragment>
       <Header />
+      <Offers />
     </Fragment>
   );
 };
