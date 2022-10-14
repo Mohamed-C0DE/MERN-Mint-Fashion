ca3779a19a671f0c5b9d18be027c9e78a367d98c
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-10-13 22:35:51 -0400

    add hero image section

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -7,6 +7,14 @@ const homepage = () => {
     <Fragment>
       <Offers />
       <Header />
+      <div className="container">
+        {/* Hero Image Section */}
+        <section className="before:bg-hero-image before:absolute before:bg-top before:bg-cover before:bg-no-repeat before:top-0 before:left-0 before:bottom-0 before:right-0 before:overflow-hidden before:opacity-95 flex justify-center content-center flex-col overflow-hidden w-full h-screen relative">
+          <h2 className="relative text-center text-3xl text-white">
+            Shop our Fall 21 Collection
+          </h2>
+        </section>
+      </div>
     </Fragment>
   );
 };
