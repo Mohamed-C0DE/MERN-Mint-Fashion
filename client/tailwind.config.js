5e6273f303df8631da5a2b884834573dbfb1a887
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-10-13 22:39:04 -0400

    add new background image class

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-image":
+          "url('https://images.pexels.com/photos/5082981/pexels-photo-5082981.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260')",
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
