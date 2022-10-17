20802b6dcd1be1898db3899a39fef97a365ff097
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-10-16 23:02:29 -0400

    add styling to card

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,8 +1,11 @@
-import { Fragment } from "react";
+import { useState, useEffect, Fragment } from "react";
 import Header from "../components/header";
 import Offers from "../components/offers";
 
 const homepage = () => {
+  // const [products, setProducts] = useState([]);
+
+  // useEffect(() => {}, []);
   return (
     <Fragment>
       <Offers />
@@ -25,7 +28,7 @@ const homepage = () => {
       {/* Best Sellers */}
       <section>
         <h2 className="text-center text-2xl pt-2">Best Sellers</h2>
-        <div className="">
+        <div className="container">
           <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
               <img
@@ -41,9 +44,9 @@ const homepage = () => {
                 </h5>
               </a>
               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                <span>
+                <span className="inline-block">
                   <svg
-                    className="w-6"
+                    className="w-3 mr-2"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 576 512"
                   >
@@ -52,25 +55,26 @@ const homepage = () => {
                 </span>
                 4.5
               </p>
-              <a
-                href="#"
-                className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Read more
-                <svg
-                  aria-hidden="true"
-                  className="ml-2 -mr-1 w-4 h-4"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
+              <div className="flex justify-between content-center">
+                <p className=" font-normal text-gray-700 flex items-center">
+                  $9.99
+                </p>
+                <a
+                  href="#"
+                  className="inline-flex items-center py-1 px-2 text-base font-medium text-center text-white bg-black rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 "
                 >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </a>
+                  View
+                  <svg
+                    aria-hidden="true"
+                    className="ml-2 -mr-1 w-4 h-4"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"></path>
+                  </svg>
+                </a>
+              </div>
             </div>
           </div>
         </div>
