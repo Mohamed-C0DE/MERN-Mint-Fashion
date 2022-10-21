92a6b68f8ce5f6e56f6f67c3b9cbfb784e4079d9
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-10-20 23:20:53 -0400

    create UI component folder

---
+++ client/src/UI/LineBreak.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LineBreak = () => {
+  return (
+    <div className="py-5 mt-5 w-5/6 mx-auto border-t-2 border-slate-200"></div>
+  );
+};
+
+export default LineBreak;
