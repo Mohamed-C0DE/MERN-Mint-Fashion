3e165a0d43905f7d6be88472348067c2051a177c
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-10-21 22:53:30 -0400

    change padding styling

--- client/src/UI/LineBreak.js
+++ client/src/UI/LineBreak.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const LineBreak = () => {
   return (
-    <div className="py-5 mt-5 w-5/6 mx-auto border-t-2 border-slate-200"></div>
+    <div className="py-2 mt-5 w-5/6 mx-auto border-t-2 border-slate-200"></div>
   );
 };
 
