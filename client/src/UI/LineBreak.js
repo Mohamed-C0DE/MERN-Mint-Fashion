66df715f44fc41cb7fb1e4536b1dd5219da8f47a
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-10-31 22:59:15 -0400

    change styling

--- client/src/UI/LineBreak.js
+++ client/src/UI/LineBreak.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 const LineBreak = () => {
-  return (
-    <div className="py-2 mt-5 w-5/6 mx-auto border-t-2 border-slate-200"></div>
-  );
+  return <hr class="py-2 w-5/6 border-gray-200 mx-auto mt-5" />;
 };
 
 export default LineBreak;
