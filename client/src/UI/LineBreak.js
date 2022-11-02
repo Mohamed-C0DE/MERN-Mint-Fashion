586b64d3dc4fc5c25916f44dbfcf7423306ee374
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-11-01 22:44:46 -0400

    fix error, class to className

--- client/src/UI/LineBreak.js
+++ client/src/UI/LineBreak.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LineBreak = () => {
-  return <hr class="py-2 w-5/6 border-gray-200 mx-auto mt-5" />;
+  return <hr className="py-2 w-5/6 border-gray-200 mx-auto mt-5" />;
 };
 
 export default LineBreak;
