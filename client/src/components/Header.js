c76f48e0c81e7cad167e516e00e3fa3da955cdb8
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-11-02 22:58:37 -0400

    change styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -18,13 +18,13 @@ const Header = () => {
         scrolledHeader
           ? `top-0 border-b border-gray-200 bg-white`
           : `bg-transparent`
-      } px-2 sm:px-4 py-0.5 fixed w-full z-20`}
+      } px-2 py-1 sm:px-4 py-0.5 fixed w-full z-20`}
     >
       <div className="container flex flex-wrap justify-between items-center mx-auto">
         <button
           data-collapse-toggle="navbar-sticky"
           type="button"
-          className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
+          className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden focus:outline-none"
           aria-controls="navbar-sticky"
           aria-expanded="false"
         >
@@ -39,12 +39,14 @@ const Header = () => {
           </svg>
         </button>
         <a href="#" className="flex items-center">
-          <span className="self-center text-2xl whitespace-nowrap">MINT</span>
+          <span className="self-center text-2xl lg:text-4xl whitespace-nowrap">
+            MINT
+          </span>
         </a>
         <div className="flex items-center md:order-2">
           <a href="https://flowbite.com/">
             <svg
-              className="hidden md:block w-4 mr-3"
+              className="hidden md:block w-5 mr-3"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 448 512"
             >
@@ -53,20 +55,20 @@ const Header = () => {
           </a>
           <a className="flex items-center" href="https://flowbite.com/">
             <svg
-              className="w-5 mr-1"
+              className="w-5 lg:w-6 mr-1"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 576 512"
             >
               <path d="M24 0C10.7 0 0 10.7 0 24S10.7 48 24 48H76.1l60.3 316.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24-10.7 24-24s-10.7-24-24-24H179.9l-9.1-48h317c14.3 0 26.9-9.5 30.8-23.3l54-192C578.3 52.3 563 32 541.8 32H122l-2.4-12.5C117.4 8.2 107.5 0 96 0H24zM176 512c26.5 0 48-21.5 48-48s-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48zm336-48c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48s48-21.5 48-48z" />
             </svg>
-            <span>(5)</span>
+            <span className="text-lg">(5)</span>
           </a>
         </div>
         <div
           className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1"
           id="navbar-sticky"
         >
-          <ul className="flex flex-col p-4 mt-4 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-lg md:font-medium md:border-0">
+          <ul className="flex flex-col p-4 mt-4 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-2xl md:border-0">
             <li>
               <a
                 href="#"
