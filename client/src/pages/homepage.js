7f1d71da56e6a821f62068ca7605705fa068c50e
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-11-05 12:42:16 -0400

    change text size when screen size is certain size

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -14,13 +14,13 @@ const Homepage = () => {
       <Header />
       {/* Hero Image Section */}
       <section className="before:bg-hero-image before:absolute before:bg-top before:bg-cover before:bg-no-repeat before:top-0 before:left-0 before:bottom-0 before:right-0 before:overflow-hidden before:opacity-95 flex justify-center content-center flex-col overflow-hidden w-full h-screen relative">
-        <h2 className="relative text-center text-5xl text-white">
+        <h2 className="relative text-center text-3xl md:text-5xl text-white">
           Shop our Fall Collection
         </h2>
         <div className="w-full flex justify-center ">
           <button
             type="button"
-            className=" relative text-1xl w-24 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg px-5 py-2.5 mr-2 mb-2"
+            className=" relative lg:text-xl w-auto text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg px-5 py-2.5 mt-2"
           >
             Shop Now
           </button>
