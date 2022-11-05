c77a4c684efeca6e97b00edb378de202036ed3a4
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-11-05 12:40:46 -0400

    change text size when screen size is certain size

--- client/src/components/offers.js
+++ client/src/components/offers.js
@@ -1,7 +1,9 @@
 const Offers = () => {
   return (
     <div className="text-center bg-stone-800">
-      <p className="text-lg text-white">FREE SHIPPING ON ORDERS OVER $60</p>
+      <p className="text-sm lg:text-lg text-white">
+        FREE SHIPPING ON ORDERS OVER $60
+      </p>
     </div>
   );
 };
